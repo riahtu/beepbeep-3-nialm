47f099e34d2231acd377c33a95b281d02cbb139c
Sylvain Hallé <shalle@acm.org>
2018-05-31 11:34:58 +0200

    Fixed UTF-8 comments

--- Source/Electric/src/electric/Learning.java
+++ Source/Electric/src/electric/Learning.java
@@ -1,6 +1,6 @@
 /*
     BeepBeep, an event stream processor
-    Copyright (C) 2008-2015 Sylvain Hall�
+    Copyright (C) 2008-2015 Sylvain Hallé
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -14,7 +14,8 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */package electric;
+ */
+package electric;
 
 import java.io.File;
 import java.io.FileInputStream;
--- Source/Electric/src/electric/PlotThatThing.java
+++ Source/Electric/src/electric/PlotThatThing.java
@@ -1,3 +1,20 @@
+/*
+    BeepBeep, an event stream processor
+    Copyright (C) 2008-2015 Sylvain Hallé
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package electric;
 
 import java.io.FileNotFoundException;
@@ -20,7 +37,7 @@ public class PlotThatThing
 		// Connect a tuple feeder to the reader
 		TupleFeeder feeder = new TupleFeeder();
 		Connector.connect(reader, feeder);
-		Plotter plot = new Plotter("TIME", "graph.pdf", "", "", "");
+		Plotter plot = new Plotter("TIME", "graph-foo.pdf", "", "", "");
 		Connector.connect(feeder,  plot);
 		plot.plot(3);
 
