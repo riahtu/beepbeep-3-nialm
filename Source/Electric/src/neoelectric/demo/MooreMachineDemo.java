35a725ef4905ade9025c65ffc0bdc22aa2064569
Sylvain Hallé <shalle@acm.org>
2018-08-30 11:17:13 -0400

    Fix to Moore machine

--- Source/Electric/src/neoelectric/demo/MooreMachineDemo.java
+++ Source/Electric/src/neoelectric/demo/MooreMachineDemo.java
@@ -8,6 +8,7 @@ import ca.uqac.lif.cep.tuples.MergeTuples;
 import ca.uqac.lif.cep.tuples.ScalarIntoTuple;
 import ca.uqac.lif.cep.tuples.Tuple;
 import ca.uqac.lif.cep.tuples.TupleMap;
+import ca.uqac.lif.cep.util.Bags;
 import neoelectric.ApplianceSignature;
 import neoelectric.ApplianceMooreMachine;
 import neoelectric.ProcessAllEnvelopes;
@@ -23,22 +24,29 @@ public class MooreMachineDemo
       Tuple on = new TupleMap();
       on.put("W1-K", 1000);
       on.put("W1-T", 400);
+      on.put("W2-K", 500);
+      on.put("W2-T", 200);
       Tuple off = new TupleMap();
       off.put("W1-T", -400);
+      off.put("W2-T", -200);
       sig = new ApplianceSignature(on, off);
     }
     SimulateAppliance appl = new SimulateAppliance(ProcessAllEnvelopesDemo.class, "data/A1", "W1", "W2");
-    ApplyFunction time_tup = new ApplyFunction(new ScalarIntoTuple("Time"));
-    Connector.connect(appl, 0, time_tup, 0);
+    
+    
     ProcessAllEnvelopes pae = new ProcessAllEnvelopes("W1", "W2");
     Connector.connect(appl, 1, pae, 0);
     Connector.connect(appl, 2, pae, 1);
     ApplianceMooreMachine amm = new ApplianceMooreMachine(sig, 100);
     Connector.connect(pae, amm);
-    Pullable p = amm.getPullableOutput();
+    ApplyFunction ta = new ApplyFunction(new Bags.ToArray(Number.class, ApplianceMooreMachine.State.class));
+    Connector.connect(appl, 0, ta, 0);
+    Connector.connect(amm, 0, ta, 1);
+    Pullable p = ta.getPullableOutput();
     while (p.hasNext())
     {
-      System.out.println(p.pull());
+      Object[] o = (Object[]) p.pull();
+      System.out.println(o[0] + "," + o[1]);
     }
   }
 
